3b41c201849313ffe1dec6f04ba1350ae9b53d48
EliGolam <elistasibusiness@gmail.com>
2022-11-03 19:36:58 +0100

    Created Migration table

---
+++ database/migrations/2022_11_03_174352_create_train_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('train', function (Blueprint $table) {
+            $table->id();
+            $table->string("companies", 50);
+            $table->string("departure_stations", 100);
+            $table->string("arrival_stations", 100);
+            $table->time("departure_times");
+            $table->time("arrival_times");
+            $table->string("train_codes", 10);
+            $table->tinyInteger("num_compartments");
+            $table->boolean("is_in_time");
+            $table->boolean("cancelled");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('train');
+    }
+}
